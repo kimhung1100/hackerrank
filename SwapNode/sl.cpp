c365fd67a47e21f5b26534eb3137ed8ceef8fd14
unknown <you@example.com>
2022-01-21 18:10:12 +0700

    first

---
+++ SwapNode/sl.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'swapNodes' function below.
+ *
+ * The function is expected to return a 2D_INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. 2D_INTEGER_ARRAY indexes
+ *  2. INTEGER_ARRAY queries
+ */
+void swap(int& a, int& b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void changeChild(int arr[]){
+    
+}
+vector<vector<int>> swapNodes(vector<vector<int>> indexes, vector<int> queries) {
+    int arr[1000];
+    fill_n(arr,1000,-1);
+    arr[0] = 1;
+    vector<vector<int>> ans;
+    for(int i = 0; i < indexes.size(); i++)
+        for (int j = 0; j < 2; j++){
+            if(indexes[i][j] != -1 ){ //has a node
+                arr[2*i + j + 1] = indexes[i][j];
+            }
+            /* code */
+        }
+    
+    for(int i = 0;i< queries.size(); i++){
+        swap(arr[2*i +1], arr[2*i + 2]);
+        bool change1 = false, change2 = false;
+        while(){
+
+        }
+        
+        
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<vector<int>> indexes(n);
+
+    for (int i = 0; i < n; i++) {
+        indexes[i].resize(2);
+
+        string indexes_row_temp_temp;
+        getline(cin, indexes_row_temp_temp);
+
+        vector<string> indexes_row_temp = split(rtrim(indexes_row_temp_temp));
+
+        for (int j = 0; j < 2; j++) {
+            int indexes_row_item = stoi(indexes_row_temp[j]);
+
+            indexes[i][j] = indexes_row_item;
+        }
+    }
+
+    string queries_count_temp;
+    getline(cin, queries_count_temp);
+
+    int queries_count = stoi(ltrim(rtrim(queries_count_temp)));
+
+    vector<int> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queries_item_temp;
+        getline(cin, queries_item_temp);
+
+        int queries_item = stoi(ltrim(rtrim(queries_item_temp)));
+
+        queries[i] = queries_item;
+    }
+
+    vector<vector<int>> result = swapNodes(indexes, queries);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        for (size_t j = 0; j < result[i].size(); j++) {
+            fout << result[i][j];
+
+            if (j != result[i].size() - 1) {
+                fout << " ";
+            }
+        }
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
